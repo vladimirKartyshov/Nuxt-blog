171d2a95484682a28970ea4befcd79c95de21824
vladimirKartyshov <vovan198080"inbox.ru>
2021-07-14 17:41:12 +0300

    Главная страница

---
+++ components/main/Post.vue
@@ -0,0 +1,19 @@
+<template>
+<article class='post'>
+
+</article>
+</template>
+
+<script>
+export default {
+  name: 'Post'
+}
+</script>
+
+<style scoped>
+.post {
+  height: 200px;
+  border: 1px solid black;
+  margin-bottom: 2rem;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,22 @@
 <template>
-  <div>Nuxt page</div>
+  <el-row type='flex' justify='center'>
+    <el-col :xs='24' :sm='18' :md='12' :lg='10'>
+    <app-post
+      v-for='post in 3'
+      :key='post'
+    />
+    </el-col>
+  </el-row>
 </template>
 
 <script>
-export default {}
+import AppPost from '@/components/main/Post'
+export default {
+  head: {
+    title: 'Главная'
+  },
+  components: {
+    AppPost
+  }
+}
 </script>
