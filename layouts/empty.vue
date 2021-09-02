7a71cf0dafba993d79889f55563456acae2d2222
vladimirKartyshov <vovan198080"inbox.ru>
2021-09-02 12:55:24 +0300

    Login Page

---
+++ layouts/empty.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class='empty-layout'>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Empty'
+}
+</script>
+
+<style scoped>
+.empty-layout {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  padding-top: 4rem;
+}
+</style>
---
+++ pages/admin/login.vue
@@ -0,0 +1,71 @@
+<template>
+  <el-card
+    shadow='always'
+    :style="{width: '500px'}"
+  >
+    <el-form
+      ref="form"
+      :model="controls"
+      :rules="rules"
+      @submit.native.prevent='onSubmit'
+    >
+      <h2>Войти в панель администратора</h2>
+
+      <el-form-item label="Login" prop="login">
+        <el-input v-model.trim="controls.login"/>
+      </el-form-item>
+
+      <div class='mb2'>
+      <el-form-item label="Password" prop="password">
+        <el-input v-model.trim="controls.password" type='password' />
+      </el-form-item>
+      </div>
+
+      <el-form-item>
+        <el-button
+          type="primary"
+          native-type='submit'
+          round
+          :loading='loading'
+        >
+          Enter
+        </el-button>
+      </el-form-item>
+    </el-form>
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  layout: 'empty',
+  data() {
+    return {
+      loading: false,
+      controls: {
+        login: '',
+        password: ''
+      },
+      rules: {
+        login: [
+          { required: true, message: 'Введите логин', trigger: 'blur' },
+        ],
+        password: [
+          { required: true, message: 'Введите пароль', trigger: 'blur' },
+          {min: 6, message: 'Пароль должен быть не менее 6 символов',
+          trigger: 'blur'}
+        ]
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log('submit')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
