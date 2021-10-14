d44348c0dadc9bc60373bc4ab180b0acec880b89
vladimirKartyshov <vovan198080"inbox.ru>
2021-10-14 13:42:00 +0300

    List of posts 1

---
+++ store/post.js
@@ -0,0 +1,16 @@
+import _id from '~/pages/post/_id'
+
+export const actions = {
+  async fetchAdmin( {} ) {
+    return await new Promise(resolve => {
+      setTimeout( () => {
+        resolve([
+          {title: 'Post 1', date: new Date(), views: 11, comments: [1, 2],
+          _id: Math.random()},
+          {title: 'Post 2', date: new Date(), views: 11, comments: [1, 2],
+            _id: Math.random()}
+        ])
+      }, 1000)
+    })
+  }
+}
